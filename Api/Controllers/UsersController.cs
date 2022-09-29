12dccc22e00ac30f38b766233b55067e6749fad2
Nnadozie Eneremadu <neneremadu@yahoo.com>
2022-09-29 12:32:46 +0100

    Api setup

---
+++ Api/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+using Api.Data;
+using Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        //method to get all the users 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+
+        }
+
+
+        //method the get user according the username
+        [HttpGet("{id}")]
+        public async Task <ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+
+        }
+
+
+    }
+}
